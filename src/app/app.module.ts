a3b06e8f2606bcc5137dbef0b1220b04e7cb195b
Tyler DeLay <dela0254@algonquinlive.com>
2022-12-14 12:32:44 -0500

    front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTicketComponent } from './components/add-ticket/add-ticket.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketDetailsComponent } from './components/ticket-details/ticket-details.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTicketComponent,
+    TicketListComponent,
+    TicketDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
