ee987b9d36fb26e7c7a5b627b9f853d047f222ea
Tyler DeLay <dela0254@algonquinlive.com>
2023-01-04 13:47:21 -0500

    home page done, need to update table appearance

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  Order: any = [];
+
+  constructor(private apiService: TicketService) {
+    this.readOrder();
+  }
+
+  ngOnInit() {}
+
+  readOrder(){
+    this.apiService.findAllTickets().subscribe((data) => {
+     this.Order = data;
+    });
+  }
 
 }
